f898531c40e805ae96be6ac8f7347dabc5ea101e
Paras <git@eparas.com>
2019-05-26 01:53:48 -0500

    Tab indents adjusted

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // exact name of default directory with all requests to hosts not in 'hosts' folder goes. Eg 127.0.0.*
 const defaultAll = "everything_else";
 
-const port = 80;								// Port to serve insecure requests.
-const portS = 443;							// Port to serve secure requests. Put 0 if you dont' want to use.
-const redirectInsecure = true;	// If 1 and portS != 0, then all insecure requests are redirected to secure. BE CAREFUL!
+const port = 80; // Port to serve insecure requests.
+const portS = 443; // Port to serve secure requests. Put 0 if you dont' want to use.
+const redirectInsecure = true; // If 1 and portS != 0, then all insecure requests are redirected to secure. BE CAREFUL!
 
 const logFileName = "logs/requestsLog.txt";
 
@@ -11,7 +11,7 @@ const logFileName = "logs/requestsLog.txt";
 const sslKeyPath = "./ssl/star_eparas_com.key";
 const sslCertPath = "./ssl/star_eparas_com.crt";
 const caPaths = ["./ssl/intermediate_domain_ca.crt"];
-const sslIgnoreHosts = ["localhost","127.0.0.1"];
+const sslIgnoreHosts = ["localhost", "127.0.0.1"];
 
 /////////////////////////////////////////////////////////////
 // Try not not change anything down here without knowledge //
@@ -19,10 +19,10 @@ const sslIgnoreHosts = ["localhost","127.0.0.1"];
 
 // get all the host names inside hosts directory
 const fs = require('fs');
-const myHostsList = [];					// will be filled automatically later, ignore
+const myHostsList = []; // will be filled automatically later, ignore
 fs.readdirSync('./hosts/').forEach(file => {
   if (file.charAt(0) == "_" || file == defaultAll) return;
-	myHostsList.push(file);
+  myHostsList.push(file);
 });
 
 var ejs = require('ejs');
@@ -31,14 +31,17 @@ var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 
 var session = require('express-session')
-var sessionOptions = { 
-	secret: 'JS2K249JSIS9S8GJ843M', 
-	cookie: { secure: true, maxAge: 3600000 },
-	saveUninitialized: false,
-	resave: false
-	//store: new FileStore()
+var sessionOptions = {
+  secret: 'JS2K249JSIS9S8GJ843M',
+  cookie: {
+    secure: true,
+    maxAge: 3600000
+  },
+  saveUninitialized: false,
+  resave: false
+  //store: new FileStore()
 };
- 
+
 var express = require('express');
 var app = express();
 
@@ -47,15 +50,15 @@ var httpServer = require('http').Server(app);
 // HTTPS Option
 var httpsOptions = {}
 var httpsServer = {}
-if (portS != 0){
-	httpsOptions = {
-		key: fs.readFileSync(sslKeyPath),
-		cert: fs.readFileSync(sslCertPath),
-		ca: []
-	};
-	for (var i = 0; i < caPaths.length; i++)
-		httpsOptions.ca.push(fs.readFileSync(caPaths[i]));
-	httpsServer = require('https').Server(httpsOptions, app);
+if (portS != 0) {
+  httpsOptions = {
+    key: fs.readFileSync(sslKeyPath),
+    cert: fs.readFileSync(sslCertPath),
+    ca: []
+  };
+  for (var i = 0; i < caPaths.length; i++)
+    httpsOptions.ca.push(fs.readFileSync(caPaths[i]));
+  httpsServer = require('https').Server(httpsOptions, app);
 }
 
 // Lets use the app 
@@ -66,39 +69,43 @@ app.engine('html', ejs.renderFile);
 // app.set('views', 'cloud/views');
 
 // Using bodyparser, session, and cookies
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(session(sessionOptions));
 app.use(cookieParser("JS2K249JSIS9S8GJ843M"));
 
 // INSECURE TO SECURE REDIRECT
 if (portS != 0 && redirectInsecure == true)
-	app.use('*', function(req, res, next) {
-		if (req.secure)
-			return next();
-		
-		for (var i = 0 ; i < sslIgnoreHosts.length; i++){
-			if (req.headers.host == sslIgnoreHosts[i]) // || (req.headers.host && req.headers.host.search && req.headers.host.search("127.0.0.") == 0))
-				return next();
-		}
-		console.log("Insecure request at " + req.headers.host + req.url + " redirected to secure.");
-		res.redirect('https://' + req.headers.host + req.url);
-	})
+  app.use('*', function(req, res, next) {
+    if (req.secure)
+      return next();
+
+    for (var i = 0; i < sslIgnoreHosts.length; i++) {
+      if (req.headers.host == sslIgnoreHosts[i]) // || (req.headers.host && req.headers.host.search && req.headers.host.search("127.0.0.") == 0))
+        return next();
+    }
+    console.log("Insecure request at " + req.headers.host + req.url + " redirected to secure.");
+    res.redirect('https://' + req.headers.host + req.url);
+  })
 
 // Logging into console, log.txt, and saving the log and defaultAll in req.log and req.defaultAll for later use
 app.use('*', (req, res, next) => {
-	var currentdate = new Date();
-	var ip = req.connection.remoteAddress || req.header('x-forwarded-for');
-	if (!fs.existsSync(logFileName.split('/')[0]))
-		fs.mkdirSync(logFileName.split('/')[0]);
-	var log = (`${req.method} on ${req.protocol}://${req.hostname}${req.originalUrl} by ${ip} at time ` 
-                + (currentdate.getMonth()+1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
-	req.log = log;
-	req.defaultAll = defaultAll;
-	console.log(log);
-	fs.appendFile(logFileName, log + '\r\n', function (err) { if (err) console.log("Couldn't log.", err);})
-	next();
+  var currentdate = new Date();
+  var ip = req.connection.remoteAddress || req.header('x-forwarded-for');
+  if (!fs.existsSync(logFileName.split('/')[0]))
+    fs.mkdirSync(logFileName.split('/')[0]);
+  var log = (`${req.method} on ${req.protocol}://${req.hostname}${req.originalUrl} by ${ip} at time ` +
+    (currentdate.getMonth() + 1) + "/" + currentdate.getDate() + "/" + currentdate.getFullYear() + " @ " +
+    currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
+  req.log = log;
+  req.defaultAll = defaultAll;
+  console.log(log);
+  fs.appendFile(logFileName, log + '\r\n', function(err) {
+    if (err) console.log("Couldn't log.", err);
+  })
+  next();
 });
 
 // loading global static files
@@ -107,22 +114,21 @@ app.use('/g_stat', express.static('./public/'));
 // loading host routers
 myHostsList.push(defaultAll);
 var myHosts = {};
-for (var n = 0; n < myHostsList.length; n++){
-	myHosts[myHostsList[n]] = require(`./hosts/${myHostsList[n]}/main`);
-	app.use(myHosts[myHostsList[n]].router);
+for (var n = 0; n < myHostsList.length; n++) {
+  myHosts[myHostsList[n]] = require(`./hosts/${myHostsList[n]}/main`);
+  app.use(myHosts[myHostsList[n]].router);
 }
 
 // if nothing above can handle the requests
-app.all("*", function(req,res) {
-	res.status(404).send('Content not found!');
+app.all("*", function(req, res) {
+  res.status(404).send('Content not found!');
 });
 
-
-
-
-
-
 // Finally, go live
-httpServer.listen(port, () => { console.log("Listening on port: " + port);});
+httpServer.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
 if (portS != 0)
-	httpsServer.listen(portS, () => {console.log("Listening on port: " + portS);});
+  httpsServer.listen(portS, () => {
+    console.log("Listening on port: " + portS);
+  });
--- hosts/_new_host/main.js
+++ hosts/_new_host/main.js
@@ -1,8 +1,7 @@
 // I didn't put these code with the app.js so that I can customize it well for each host
-
 const logLocal = true;
-const showServedPath = 1;	// Not implemented currently, logs what was served for current request
-const homePages = ["index.html", "home.html"];	// What to consider homepage inside pages dir
+const showServedPath = 1; // Not implemented currently, logs what was served for current request
+const homePages = ["index.html", "home.html"]; // What to consider homepage inside pages dir
 const defaultNotFound = "unexpected.html";
 const logFileDir = "logs";
 const logFileName = logFileDir + "/requestsLog.txt";
@@ -22,57 +21,65 @@ var cwd = `./hosts/${hostname}`;
 
 // gateway function for host filtering
 app.use(function(req, res, next) {
-	if (hostname != req.defaultAll && hostname != req.hostname)
-		next('router');	
-	else
-		next();
+  if (hostname != req.defaultAll && hostname != req.hostname)
+    next('router');
+  else
+    next();
 });
 
 // logging into the local log file, only if logLocal is True
 if (logLocal == true) {
-	if (!fs.existsSync(`${cwd}/${logFileDir}`))
-		fs.mkdirSync(`${cwd}/${logFileDir}`);
-	app.use((req, res, next) => {
-		var logFile = `${cwd}/${logFileName}`;
-		fs.appendFile(logFile, req.log + '\r\n', function (err) { if (err) console.log("Couldn't log: ", err);});
-		next();
-	});
+  if (!fs.existsSync(`${cwd}/${logFileDir}`))
+    fs.mkdirSync(`${cwd}/${logFileDir}`);
+  app.use((req, res, next) => {
+    var logFile = `${cwd}/${logFileName}`;
+    fs.appendFile(logFile, req.log + '\r\n', function(err) {
+      if (err) console.log("Couldn't log: ", err);
+    });
+    next();
+  });
 }
 
 // serving static files from public directory (syles, scripts, images, and file)
-app.use(express.static(`${cwd}/public`));  // to separate: app.use('/styles', express.static(`${cwd}/public/styles`));
+app.use(express.static(`${cwd}/public`)); // to separate: app.use('/styles', express.static(`${cwd}/public/styles`));
 
 // for page at '/' is served by first available file from homePages dir
-var homePage = "";	// default
-for (var i = 0; i < homePages.length; i++){
-	if (fs.existsSync(`${cwd}/public/${homePages[i]}`)) {	// homepage available
-		var homePage = `${cwd}/public/${homePages[i]}`;
-		app.use('/$/', function(req, res){
-			res.status(200).sendFile(homePage, {root: "./"});
-		});
-		break;
-	}
+var homePage = ""; // default
+for (var i = 0; i < homePages.length; i++) {
+  if (fs.existsSync(`${cwd}/public/${homePages[i]}`)) { // homepage available
+    var homePage = `${cwd}/public/${homePages[i]}`;
+    app.use('/$/', function(req, res) {
+      res.status(200).sendFile(homePage, {
+        root: "./"
+      });
+    });
+    break;
+  }
 }
 
 // **************************************** //
 // Your middlewares or anything can go here
 // ---------------------------------------- //
 if (fs.existsSync(`${cwd}/routes/router.js`)) {
-	app.use(require(`./routes/router`));
+  app.use(require(`./routes/router`));
 }
 // ---------------------------------------- //
 // **************************************** //
 
 // for lost page or anything anything else not handled above
-if (fs.existsSync(`${cwd}/public/${defaultNotFound}`)){
-	app.all("*", function(req,res) {
-		res.status(404).sendFile(`${cwd}/public/${defaultNotFound}`, {root: "./"});
-	});
+if (fs.existsSync(`${cwd}/public/${defaultNotFound}`)) {
+  app.all("*", function(req, res) {
+    res.status(404).sendFile(`${cwd}/public/${defaultNotFound}`, {
+      root: "./"
+    });
+  });
 } else {
-	app.all("*", function(req,res, next) {
-		res.status(404).send('Content not found!');
-	});
+  app.all("*", function(req, res, next) {
+    res.status(404).send('Content not found!');
+  });
 }
 
 // Export
-module.exports = {router: app}
+module.exports = {
+  router: app
+}
